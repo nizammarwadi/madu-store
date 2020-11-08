bd4cdd7245b2c290de5dbc42d5984dbe5ad15b72
nizammarwadi <nizammarwadi94@gmail.com>
2020-11-08 11:05:39 +0700

    update style benefi section

--- components/organisms/BenefitSection.vue
+++ components/organisms/BenefitSection.vue
@@ -9,11 +9,11 @@
             </div>
              <img src="~/assets/image/registration.png" alt="" class="whyus-one-img">
         </div>
-        <div class="whyus-two">
-             <img src="~/assets/image/registration.png" alt="" class="whyus-two-img">
-            <div class="whyus-content-two">
-                <h3 class="whyus-two-title">Harga Terjangkau dan Terpercaya</h3>
-                <p class="whyus-two-content">Kualitas dari madu kami jangan anda ragukan lagi, dikarenakan kemurnian dan kesempurnaan rasa dan khasiatnya. Sehingga dapat membuat anda merasakan kemurnian dan keaslian madu dari kami tanpa harus ragu.</p>
+        <div class="whyus-one">
+             <img src="~/assets/image/registration.png" alt="" class="whyus-one-img">
+            <div class="whyus-content-one">
+                <h3 class="whyus-one-title">Harga Terjangkau dan Terpercaya</h3>
+                <p class="whyus-one-content">Kualitas dari madu kami jangan anda ragukan lagi, dikarenakan kemurnian dan kesempurnaan rasa dan khasiatnya. Sehingga dapat membuat anda merasakan kemurnian dan keaslian madu dari kami tanpa harus ragu.</p>
             </div>
         </div>
         <div class="whyus-one">
@@ -76,31 +76,8 @@ body {
     text-align: justify;
     font-size: 17px;
     font-weight: 400;
-
 }
 .whyus-one-img {
-    max-width: 400px;
-}
-.whyus-two {
-    padding-top: 50px;
-    padding-left: 80px;
-    width: 100%!important;
-    text-align: justify;
-    display: flex;
-    justify-content: space-around;
-}
-.whyus-two-title {
-    margin-top: 40px;
-    font-size: 32px;
-    font-weight: 700;
-}
-.whyus-content-two {
-    width: 500px;
-    font-size: 17px;
-    font-weight: 400;
-
-}
-.whyus-two-img {
     max-width: 400px;
 }
 @media(max-width: 678px){
@@ -114,20 +91,9 @@ body {
         padding: 0px 10px;
         margin: 0px auto;
     }
-    .whyus-two{
-        display: block;
-        width: 90%;
-        padding: 0px 10px;
-        margin: 0px auto;
-    }
     .whyus-content-one{
         width: 100%;
         margin: 0px 0px;
-        padding: 0px;
-    }
-    .whyus-content-two{
-        width: 100%;
-        margin: 0px;
         padding: 0px;
     }
     .whyus-one-img {
